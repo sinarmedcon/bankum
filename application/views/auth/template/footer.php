7bf639d391592a4732e19a78e000c7c3fad91e08
Mentorlink <47414125+aliftech@users.noreply.github.com>
2019-09-25 07:16:48 +0700

    add view repository in application

---
+++ application/views/auth/template/footer.php
@@ -0,0 +1,8 @@
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?= base_url();?>assets/js/jquery.js"></script>
+    <script src="<?= base_url();?>assets/js/popover.js"></script>
+    <script src="<?= base_url();?>assets/js/bootstrap.js"></script>
+  </body>
+</html>
