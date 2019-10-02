b20ba282f367b7c26c523f18f9cf8c76dab5013d
Wahyu K. A <47414125+aliftech@users.noreply.github.com>
2019-10-02 20:35:17 +0700

    Update footer.php

--- application/views/auth/template/footer.php
+++ application/views/auth/template/footer.php
@@ -1,4 +1,78 @@
+<div class="jumbotron jumbotron-fluid bg-dark mb-0">
+    <div class="container">
+        
+        <div class="row">
+        
+            <div class="col-md-4">
+            
+                <a href="#" class="link"><img src="<?= base_url();?>assets/image/29.png" width="225" height="75"></a><br><br>
+                <a href="#" class="link"><img src="<?= base_url();?>assets/image/playstore.png" width="225" height="75"></a><br><br>
+                <a href="#" class="link"><img src="<?= base_url();?>assets/image/appstore.png" width="225" height="75"></a>
+            
 
+            </div>
+
+            <div class="col-md-2">
+            
+                <p class="text-white mt-3"><b>Layanan Kami</b></p>
+                <p><a href="#" class="text-white link">Bantuan Hukum</a></p>
+                <p><a href="#" class="text-white link">Layanan Hukum</a></p>
+                <p><a href="#" class="text-white link">Legalitas</a></p>
+                <p><a href="#" class="text-white link">Paralegal</a></p>
+                <p><a href="#" class="text-white link">Berita Hukum</a></p>
+                <p><a href="#" class="text-white link">UU & Peraturan</a></p>
+            
+            </div>
+
+            <div class="col-md-2">
+            
+                <p class="text-white mt-3"><b>Tentang Bankum</b></p>
+                <p><a href="<?= base_url();?>auth/blog" class="text-white link">Blog</a></p>
+                <p><a href="#" class="text-white link">Karir</a></p>
+                <p><a href="#" class="text-white link">Volunter / Relawan</a></p>
+                <p><a href="#" class="text-white link">Mitra Kami</a></p>
+                <p><a href="#" class="text-white link">Tentang Kami</a></p>
+            
+            </div>
+
+            <div class="col-md-2">
+            
+                <p class="text-white mt-3"><b>Bantuan</b></p>
+                <p><a href="#" class="text-white link">Kontak Kami</a></p>
+                <p><a href="#" class="text-white link">Pengaduan</a></p>
+                <p><a href="#" class="text-white link">Kebijakan Privasi</a></p>
+                <p><a href="#" class="text-white link">Syarat & Ketentuan</a></p>
+            
+            </div>
+
+            <div class="col-md-2">
+            
+                <p class="text-white mt-3"><b>Temukan Kami</b></p>
+                <p><a href="#" class="text-white link"><img src="<?= base_url();?>assets/image/fb.png" width="25" height="25"> Facebook</a></p>
+                <p><a href="#" class="text-white link"><img src="<?= base_url();?>assets/image/twitter.png" width="25" height="25"> Twitter</a></p>
+                <p><a href="#" class="text-white link"><img src="<?= base_url();?>assets/image/linkedin.png" width="25" height="25"> Linkin</a></p>
+                <p><a href="#" class="text-white link"><img src="<?= base_url();?>assets/image/youtube.png" width="25" height="25"> YouTube</a></p>
+                <p><a href="#" class="text-white link"><img src="<?= base_url();?>assets/image/ig.png" width="25" height="25"> Instagram</a></p>
+            
+            </div>
+        
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="text-center">
+            <div class="row">
+                <div class="col-md-12 ml-auto mr-auto">
+                    <div class="dropdown-divider"></div>
+                    <small class="text-white small">Copyright @2019, Bankum All Rights Reserved.</small><br>
+                    <small class="text-white mb-3 small">Bankum merupakan merek milik PT Aplikasi Untuk Bangsa dan telah didaftarkan pada Dirjen Hak Kekayaan Intelektual</small>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="<?= base_url();?>assets/js/jquery.js"></script>
