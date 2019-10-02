2d7edb3a0688474600400c6ee2ba775f69bc5cf5
Wahyu K. A <47414125+aliftech@users.noreply.github.com>
2019-10-02 20:29:00 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -2,11 +2,33 @@
 
 
 class Auth extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('auth_model', 'auth');
+    }
+
     public function index()
     {
         $t['title'] = 'Selamat Datang di Bankum';
         $this->load->view('auth/template/header', $t);
         $this->load->view('auth/index/main');
+        $this->load->view('auth/template/footer');
+    }
+
+    public function blog()
+    {
+        $t['title'] = 'Bankum :: Blog';
+        $this->load->view('auth/template/header', $t);
+        $this->load->view('auth/index/blog');
+        $this->load->view('auth/template/footer');
+    }
+
+    public function gabung()
+    {
+        $t['title'] = 'Bankum :: Gabung';
+        $this->load->view('auth/template/header', $t);
+        $this->load->view('auth/index/gabung');
         $this->load->view('auth/template/footer');
     }
 }
