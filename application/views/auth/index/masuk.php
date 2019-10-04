d3399d901f2e2ebd346edfdb572500642e47f0d9
Wahyu K. A <47414125+aliftech@users.noreply.github.com>
2019-10-04 23:55:31 +0700

    Add files via upload

---
+++ application/views/auth/index/masuk.php
@@ -0,0 +1,48 @@
+<br>
+    <br>
+
+<div class="jumbotron jumbotron-fluid mb-0" style="background:linear-gradient(to top right, #B0CDD9, #EFF288, #F2C3A7)">
+    <div class="container">
+        <div class="row">
+        
+            <div class="col-md-6">
+            
+                <h1 class="text-dark"><b>Selamat Datang Kembali</b></h1>
+                <p class="lead text-dark">Silakan masuk untuk membagikan informasi dan ide-ide menarik Anda seputar hukum di Indonesia.</p>
+            
+            </div>
+
+
+            <div class="col-md-6">
+            
+                <form action="">
+                
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Alamat Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                        <small id="emailHelp" class="form-text text-muted">Kami tidak akan memberikan alamat email Anda Kepada siapapun</small>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Kata Sandi</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Kata sandi akun anda">
+                    </div>
+
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Ingat saya</label>
+                    </div>
+
+                    <div class="text-center">
+                    
+                        <button type="button" class="btn btn-primary">Masuk</button>
+                    
+                    </div>
+                
+                </form>
+            
+            </div>
+        
+        </div>
+    </div>
+</div>
