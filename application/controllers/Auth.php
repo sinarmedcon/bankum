32438039e1bc3d2174147c8b64d7da95e10f1c54
Wahyu K. A <47414125+aliftech@users.noreply.github.com>
2019-10-04 23:56:22 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -2,6 +2,11 @@
 
 
 class Auth extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('auth_model', 'auth');
+    }
 
     public function index()
     {
