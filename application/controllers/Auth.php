56b7d51ee7bfa6429526a1db8ab96bd4ab3e4ab2
Wahyu K. A <47414125+aliftech@users.noreply.github.com>
2019-10-25 21:36:25 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,6 +6,7 @@ class Auth extends CI_Controller{
     {
         parent::__construct();
         $this->load->model('auth_model', 'auth');
+        $this->load->library('form_validation');
     }
 
     public function index()
@@ -32,11 +33,6 @@ class Auth extends CI_Controller{
         $this->load->view('auth/template/footer');
     }
 
-    public function admLogEnter()
-    {
-        
-    }
-
     public function legalitas()
     {
         $t['title'] = 'Bankum :: Legalitas';
@@ -47,10 +43,65 @@ class Auth extends CI_Controller{
 
     public function masuk()
     {
-        $t['title'] = 'Bankum :: Masuk';
-        $this->load->view('auth/template/header', $t);
-        $this->load->view('auth/index/masuk');
-        $this->load->view('auth/template/footer');
+        if($this->input->cookie('blog_admin'))
+        {
+            redirect('blog');
+        } else {
+            $t['title'] = 'Bankum :: Masuk';
+            $this->load->view('auth/template/header', $t);
+            $this->load->view('auth/index/masuk');
+            $this->load->view('auth/template/footer');
+        }
+    }
+
+    public function proses_masuk()
+    {
+        $config = [
+            [
+                'field' => 'username',
+                'label' => 'Username',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'password',
+                'label' => 'Kata Sandi',
+                'rules' => 'required'
+            ]
+        ];
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run() == FALSE)
+        {
+            $t['title'] = 'Validation Error!';
+            $this->load->view('auth/template/header', $t);
+            $this->load->view('auth/index/masuk');
+            $this->load->view('auth/template/footer');
+        } else {
+            $username = md5($this->input->post('username'));
+            $password = md5($this->input->post('password'));
+
+            $cek = $this->auth->validasi_login($username, $password);
+
+            if($cek->num_rows()>0)
+            {
+               foreach($cek->result() as $row)
+               {
+                   $userid = $row->userid;
+                   $cookie = [
+                    'name' => 'blog_admin',
+                    'value' => $userid,
+                    'expire' => '86400',
+                    'secure' => TRUE
+                    ];
+                    $this->input->set_cookie($cookie);
+                    redirect('blog');
+               } 
+            } else {
+                $t['title'] = 'Gagal Masuk ! Silakan Coba Lagi Nanti';
+                $this->load->view('auth/template/header', $t);
+                $this->load->view('auth/index/masuk');
+                $this->load->view('auth/template/footer');
+            }
+        }
     }
 
     public function tentang_kami()
@@ -60,4 +111,13 @@ class Auth extends CI_Controller{
         $this->load->view('auth/index/tentang_kami');
         $this->load->view('auth/template/footer');
     }
+
+    public function paralegal()
+    {
+        $t['title'] = 'Bankum :: Paralegal';
+        $this->load->view('auth/template/header', $t);
+        $this->load->view('auth/index/paralegal');
+        $this->load->view('auth/template/footer');
+    }
+
 }
