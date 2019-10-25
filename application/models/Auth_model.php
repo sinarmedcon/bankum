9d7ed36070973f210caea13cd35b9f69edb5bf6d
Wahyu K. A <47414125+aliftech@users.noreply.github.com>
2019-10-25 21:39:33 +0700

    Update Auth_model.php

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -2,4 +2,9 @@
 
 class Auth_model extends CI_Model{
 
+    public function validasi_login($username, $password)
+    {
+        $this->db->select('*');
+        return $this->db->get_where('blogger', array('username' => $username, 'password' => $password));
+    }
 }
